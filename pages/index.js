95cb23993f3772be9672bd5ff1e4bbe3bb2c989e
Jeff Shen <jshen2014@hotmail.com>
2020-09-26 15:14:58 -0400

    change layout to rows and columns

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,17 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className="column">
+          <h1 className="title">
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <p className="description">
+            Get started by editing <code>pages/index.js</code>
+          </p>
+        </div>
 
-        <div className="grid">
+        <div className="column">
           <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
