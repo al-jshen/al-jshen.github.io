7094e91f9ead78a02981261f1e4954a7d76f5a18
Jeff Shen <jshen2014@hotmail.com>
2020-09-26 20:32:29 -0400

    nicer linking (whole card is clickable)

--- pages/projects.js
+++ pages/projects.js
@@ -3,41 +3,30 @@ import Link from "next/link";
 export default function Projects() {
   return (
     <div className="column">
-      <div className="card">
-        <h3>
-          <Link href="/projects/test">
-            <a className="clickable">Testing &rarr;</a>
-          </Link>
-        </h3>
-        <p>Find in-depth information about Next.js features and API.</p>
-      </div>
-
-      <div className="card">
-        <h3>
-          <Link href="/projects/test">
-            <a>Testing &rarr;</a>
-          </Link>
-        </h3>
-        <p>Find in-depth information about Next.js features and API.</p>
-      </div>
-
-      <div className="card">
-        <h3>
-          <Link href="/projects/test">
-            <a>Testing &rarr;</a>
-          </Link>
-        </h3>
-        <p>Find in-depth information about Next.js features and API.</p>
-      </div>
-
-      <div className="card">
-        <h3>
-          <Link href="/projects/test">
-            <a>Testing &rarr;</a>
-          </Link>
-        </h3>
-        <p>Find in-depth information about Next.js features and API.</p>
-      </div>
+      <Link href="/projects/test" className="card">
+        <a className="card">
+          <h3>Testing &rarr;</h3>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+      </Link>
+      <Link href="/projects/test" className="card">
+        <a className="card">
+          <h3>Testing &rarr;</h3>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+      </Link>
+      <Link href="/projects/test" className="card">
+        <a className="card">
+          <h3>Testing &rarr;</h3>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+      </Link>
+      <Link href="/projects/test" className="card">
+        <a className="card">
+          <h3>Testing &rarr;</h3>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+      </Link>
     </div>
   );
 }
