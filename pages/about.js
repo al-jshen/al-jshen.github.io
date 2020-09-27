569163c745cd006146411933bb3fa2e00dde99be
Jeff Shen <jshen2014@hotmail.com>
2020-09-26 22:27:38 -0400

    better paragraph spacing; added filler text for testing

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,19 @@ export default function About() {
   return (
     <div className="column">
       <h1 className="title">Jeff Shen</h1>
-      <p>testing</p>
+      <p>
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo.
+      </p>
+      <p>
+        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+        fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem
+        sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+        sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
+        tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+      </p>
     </div>
   );
 }
