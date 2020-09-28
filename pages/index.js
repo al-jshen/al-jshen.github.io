ccb784c7357051e3eab28a51ea280bb32e2c15b0
Jeff Shen <jshen2014@hotmail.com>
2020-09-28 15:22:03 -0400

    add font awesome stylesheet, add misc section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
-import Projects from "./projects";
 import About from "./about";
+import Projects from "./projects";
+import Misc from "./misc";
 
 export default function Home() {
   return (
@@ -8,13 +9,20 @@ export default function Home() {
       <Head>
         <title>Jeff Shen</title>
         <link rel="icon" href="/logo.svg" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       <main>
         <div className="row">
           <About />
-          <Projects />
+          <div className="column">
+            <Projects />
+            <Misc />
+          </div>
         </div>
       </main>
     </div>
