d99478a9a6a7e60e50ced5432cf23ec6cf5c8ed2
Jeff Shen <jshen2014@hotmail.com>
2020-10-02 21:09:52 -0400

    no link is better than wrong link

--- pages/about.js
+++ pages/about.js
@@ -9,18 +9,13 @@ export default function About() {
       </div>
       <p>
         I am currently doing research in the field of astrostatistics under the
-        supervision of{" "}
-        <a href="//astro.utoronto.ca/~eadie/cover.html">Gwen Eadie</a>{" "}
-        and <a href="//www.cita.utoronto.ca/~murray/">Norm Murray</a>. My focus
-        is on applying Bayesian statistics to study the mass distribution of the
-        Milky Way using kinematic data from tracer objects.
+        supervision of Gwen Eadie and Norm Murray. My focus is on applying
+        Bayesian statistics to study the mass distribution of the Milky Way
+        using kinematic data from tracer objects.
       </p>
       <p>
-        Before this, I was a summer research student supervised by{" "}
-        <a href="https://www.dunlap.utoronto.ca/dunlap-people/dr-allison-man/">
-          Allison Man
-        </a>
-        . I studied molecular gas in faint high redshift galaxies by leveraging
+        Before this, I was a summer research student supervised by Allison Man.
+        I studied molecular gas in faint high redshift galaxies by leveraging
         gravitational lensing.
       </p>
       <p>
